6efce78d5237bba1a586a4c43d5d54bfbfc63736
joshuak07 <67910818+joshuak07@users.noreply.github.com>
2020-09-15 10:51:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var zombiei,bulleti,guni,backgrouu;
+
+function preload(){
+   zombiei=loadImage("zombie-155403.png") 
+   bulleti=loadImage("bullet-26507_1280.png")
+   guni=loadImage("gun_PNG1373.png");
+   backgrouu=loadImage("ceC8tM.jpg")
+}
+
+function setup(){
+createCanvas(displayWidth,displayHeight);
+}
+
+
+function draw(){
+background(backgrouu)
+}
+
+
+
